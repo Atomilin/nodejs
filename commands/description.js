a0a4c31eb83bab29c6067f88aaff4e79c896c1db
Atomilin <50828840+Atomilin@users.noreply.github.com>
2019-09-24 15:14:36 +0300

    homework_1_callback

--- commands/description.js
+++
@@ -1,9 +0,0 @@
-const packageInfo = require(`../package.json`);
-
-module.exports = {
-  name: `--description`,
-  description: `typing the description of application`,
-  execute () {
-    console.log(`${packageInfo.description}`);
-  }
-};
